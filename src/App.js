2f26fad41e4e35918ec6149df3892231e8839ad1
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2022-01-22 10:13:42 +0530

    Adding Background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.scss'
-
 import AddProduct from './Pages/AddProducts/AddProduct'
+
+import './App.scss'
 
 function App() {
     return (
         <div className="App">
-            <AddProduct />
+            <img
+                src={require('./assets/Background.jpg')}
+                alt="Background"
+                className="bg_img"
+            />
+            {/* <AddProduct /> */}
         </div>
     )
 }
