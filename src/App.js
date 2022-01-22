15204dd63f0abd1e13d99e0440be7b7e9e494ed6
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2022-01-22 09:35:15 +0530

    Setting up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.scss'
 
+import AddProduct from './Pages/AddProducts/AddProduct'
+
 function App() {
-    return <div className="App"></div>
+    return (
+        <div className="App">
+            <AddProduct />
+        </div>
+    )
 }
 
 export default App
