e2ec5d18209e6236e1d8a03708d410996fb74b51
Bhavesh588 <sbhavesh588@gmail.com>
2022-03-23 20:30:11 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <div className="App">
             <Routes>
                 <Route
-                    path="/comprafaciladmin/products"
+                    path="/comprafaciladmin/"
                     exact
                     element={<ShowProduct />}
                 />
